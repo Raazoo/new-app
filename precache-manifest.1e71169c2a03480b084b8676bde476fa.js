fe020cb09c17a35aa3cf87bd3343f229f192bb22
RajuSaha1693 <59817023+RajuSaha1693@users.noreply.github.com>
2020-09-30 11:42:58 +0530

    Updates

---
+++ precache-manifest.1e71169c2a03480b084b8676bde476fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a696009b64c32d47301f9dac136d6da3",
+    "url": "/new-app/index.html"
+  },
+  {
+    "revision": "95bd83cfa709af5712e6",
+    "url": "/new-app/static/css/main.0662ecdc.chunk.css"
+  },
+  {
+    "revision": "3354ab1e5e9f13df9151",
+    "url": "/new-app/static/js/2.9ea790fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/new-app/static/js/2.9ea790fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95bd83cfa709af5712e6",
+    "url": "/new-app/static/js/main.927bf226.chunk.js"
+  },
+  {
+    "revision": "b1be377fc82eb514832c",
+    "url": "/new-app/static/js/runtime-main.7cab8244.js"
+  }
+]);
